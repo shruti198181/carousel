87914121c045c90fba5735ccd2a9836f05da721d
shruti198181 <shahshruti503@gmail.com>
2025-05-06 15:44:30 +0530

    Your commit message here

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,18 @@ import 'owl.carousel/dist/assets/owl.theme.default.css'
 import './App.css';
 function App () {
 return (
-  <Carousel className="owl-theme" margin={10}  nav autoplay loop>  
-    <div className="item">
-      <img src="https://media.istockphoto.com/id/519330110/photo/taj-mahal-agra-india-monument-of-love-in-blue-sky.jpg?s=612x612&w=0&k=20&c=Uma6Q7KduznA6jUKcSquFP1iHHiw8UXcZEYVLONrmaQ="/>
-      </div>
-    <div className="item">
-      <img src="https://media.istockphoto.com/id/1341288649/photo/75mpix-panorama-of-beautiful-mount-ama-dablam-in-himalayas-nepal.jpg?s=612x612&w=0&k=20&c=0xb_bb-NBIxjiJL_kqY-o3dCjv2PmKFZfRjHcVEijDc="/>
-      </div>
-    <div className="item">
-      <img src="https://media.istockphoto.com/id/1347088244/photo/kerala-most-beautiful-place-of-india.jpg?s=612x612&w=0&k=20&c=oBVJ6rUfq1YOua_4Oqhj0B1TFwcMFhniaysLJEN_eh8="/>
-      </div>
-  </Carousel>
+
+ <Carousel className="owl-theme" margin={10}  nav autoplay loop>  
+     <div className="item">
+       <img src="https://media.istockphoto.com/id/519330110/photo/taj-mahal-agra-india-monument-of-love-in-blue-sky.jpg?s=612x612&w=0&k=20&c=Uma6Q7KduznA6jUKcSquFP1iHHiw8UXcZEYVLONrmaQ="/>
+       </div>
+     <div className="item">
+       <img src="https://media.istockphoto.com/id/1341288649/photo/75mpix-panorama-of-beautiful-mount-ama-dablam-in-himalayas-nepal.jpg?s=612x612&w=0&k=20&c=0xb_bb-NBIxjiJL_kqY-o3dCjv2PmKFZfRjHcVEijDc="/>
+       </div>
+     <div className="item">
+       <img src="https://media.istockphoto.com/id/1347088244/photo/kerala-most-beautiful-place-of-india.jpg?s=612x612&w=0&k=20&c=oBVJ6rUfq1YOua_4Oqhj0B1TFwcMFhniaysLJEN_eh8="/>
+       </div>
+   </Carousel>
 )
 }
-export default App;
+export default App
