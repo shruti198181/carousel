97933ef2ea832ec2e5ade098a40818ecbc525395
shruti198181 <shahshruti503@gmail.com>
2025-05-06 12:01:14 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,20 @@
-import './App.css'
-import {OwlCarousel} from 'react-owl-carousel';
-import 'owl.carousel/dist/assets/owl.carousel.css';
-import 'owl.carousel/dist/assets/owl.theme.default.css';
-
-function App() {
-
-  return (
-  <OwlCarousel className='owl-theme' margin={10}  autoplay loop autoplayTimeout={500}>
-  <div className='item'> 
-     <img src="https://media.istockphoto.com/id/1252711563/photo/green-hills-of-lonavala-shot-during-the-rainy-season.jpg?s=2048x2048&w=is&k=20&c=bp_E0Jsl68JklnXNyIsDybPMOB7pi8txnLXUoqVgY9o="  alt="img"/> 
-     </div>
-  <div className='item'> 
-     <img src="https://media.istockphoto.com/id/1160316337/photo/tea-plantations.jpg?s=612x612&w=0&k=20&c=5gj1mezzy3cHlCQcqiOIM8pLBLeMyWoxW3e_R57eyv8="/> 
-     </div>
-  <div className='item'>
-      <img src="https://media.istockphoto.com/id/1252711563/photo/green-hills-of-lonavala-shot-during-the-rainy-season.jpg?s=2048x2048&w=is&k=20&c=bp_E0Jsl68JklnXNyIsDybPMOB7pi8txnLXUoqVgY9o="/> 
+import Carousel from 'react-owl-carousel'
+import 'owl.carousel/dist/assets/owl.carousel.css' 
+import 'owl.carousel/dist/assets/owl.theme.default.css'
+import './App.css';
+function App () {
+return (
+  <Carousel className="owl-theme" margin={10}  nav autoplay loop>  
+    <div className="item">
+      <img src="https://media.istockphoto.com/id/519330110/photo/taj-mahal-agra-india-monument-of-love-in-blue-sky.jpg?s=612x612&w=0&k=20&c=Uma6Q7KduznA6jUKcSquFP1iHHiw8UXcZEYVLONrmaQ="/>
       </div>
-<div className="item">
-  <img src="https://media.istockphoto.com/id/1252711563/photo/green-hills-of-lona
-vala-shot-during-the-rainy-season.jpg?s=2048x2048&w=is&k=20&c=bp_E0Jsl68JklnXNyIsDybPMOB7pi8txnLXUoqVgY9o="/>
-</div>    
-   </OwlCarousel>
-
-  )
+    <div className="item">
+      <img src="https://media.istockphoto.com/id/1341288649/photo/75mpix-panorama-of-beautiful-mount-ama-dablam-in-himalayas-nepal.jpg?s=612x612&w=0&k=20&c=0xb_bb-NBIxjiJL_kqY-o3dCjv2PmKFZfRjHcVEijDc="/>
+      </div>
+    <div className="item">
+      <img src="https://media.istockphoto.com/id/1347088244/photo/kerala-most-beautiful-place-of-india.jpg?s=612x612&w=0&k=20&c=oBVJ6rUfq1YOua_4Oqhj0B1TFwcMFhniaysLJEN_eh8="/>
+      </div>
+  </Carousel>
+)
 }
-
 export default App;
